38cf53199d1e52d941c79e42bed664fd0a722b7d
FFede0 <federica.filippini@unimib.it>
2024-11-05 17:10:19 +0100

    number of components and instances can be specified as parameters

--- example_applications/run_s4air_vs_bcpc.py
+++ example_applications/run_s4air_vs_bcpc.py
@@ -14,6 +14,7 @@ Copyright 2021 AI-SPRINT
  limitations under the License.
 """
 from typing import Tuple
+from parse import parse
 import argparse
 import json
 import os
@@ -30,6 +31,17 @@ def parse_arguments() -> argparse.Namespace:
     "--application_dir", 
     help="Path to the base folder", 
     type=str
+  )
+  parser.add_argument(
+    "-n", "--n_components", 
+    help="Number of components to consider", 
+    type=int,
+    nargs="+"
+  )
+  parser.add_argument(
+    "-i", "--n_instances", 
+    help="Number of instances to consider (for each scenario)",
+    default="all"
   )
   args, _ = parser.parse_known_args()
   return args
@@ -87,7 +99,7 @@ def write_config_file(base_folder: str, config: dict):
   return config_file
 
 
-def main(base_folder: str):
+def main(base_folder: str, n_components_list: list, n_instances: int):
   # load thresholds
   all_thresholds = load_thresholds(
     os.path.join(base_folder, "thresholds.json")
@@ -96,25 +108,27 @@ def main(base_folder: str):
   successful = []
   failed = []
   for scenario, instances in all_thresholds.items():
-    print(f"Scenario: {scenario}")
-    # loop over all instances
-    for instance, thresholds in instances.items():
-      print(f"  Instance: {instance}")
-      instance_folder = os.path.join(base_folder, scenario, instance)
-      # loop over all thresholds
-      for threshold in thresholds:
-        print(f"    threshold: {threshold}")
-        # write configuration file
-        config, log_file = update_config(instance_folder, threshold)
-        config_file = write_config_file(base_folder, config)
-        # run space4ai-r
-        exe = "/home/SPACE4AI-R/s4ai-r-optimizer/BUILD/apps/s4air_exe"
-        command = f"{exe} {config_file} > {log_file} 2>&1"
-        out = os.system(command)
-        if out == 0:
-          successful.append((scenario, instance, threshold))
-        else:
-          failed.append((scenario, instance, threshold))
+    if int(parse("{}Components", scenario)[0]) in n_components_list:
+      print(f"Scenario: {scenario}")
+      # loop over all instances
+      for instance, thresholds in instances.items():
+        if int(parse("Ins{}", instance)[0]) < n_instances:
+          print(f"  Instance: {instance}")
+          instance_folder = os.path.join(base_folder, scenario, instance)
+          # loop over all thresholds
+          for threshold in thresholds:
+            print(f"    threshold: {threshold}")
+            # write configuration file
+            config, log_file = update_config(instance_folder, threshold)
+            config_file = write_config_file(base_folder, config)
+            # run space4ai-r
+            exe = "/home/SPACE4AI-R/s4ai-r-optimizer/BUILD/apps/s4air_exe"
+            command = f"{exe} {config_file} > {log_file} 2>&1"
+            out = os.system(command)
+            if out == 0:
+              successful.append((scenario, instance, threshold))
+            else:
+              failed.append((scenario, instance, threshold))
   # write list of successful/failed tests
   with open(os.path.join(base_folder, "successful.txt"), "w") as ostream:
     for exp in successful:
@@ -127,4 +141,8 @@ def main(base_folder: str):
 if __name__ == "__main__":
   args = parse_arguments()
   base_folder = args.application_dir
-  main(base_folder)
+  n_components_list = args.n_components
+  n_instances = args.n_instances
+  if str(n_instances) != "all":
+    n_instances = int(n_instances)
+  main(base_folder, n_components_list, n_instances)
